f78206e336a88da71ead442fea3bf3adb16b0e62
Bruno Siqueira Pedro Melo <86988869+brd3v@users.noreply.github.com>
2024-05-15 21:44:50 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+   <div className='header'><h2> 🔥 Aumente Seu Desejo com Libid Gel! Compre Agora pelo WhatsApp e Desfrute de uma Vida Sexual Mais Intensa!</h2> 
+   <img src='/logo512.png'></img>
+   </div>
+   <div className='footer'>
+      <a href='https://api.whatsapp.com/send?phone=5527998156485&text=Ol%C3%A1%20Gostaria%20de%20adquirir%20o%20melhor%20pre%C3%A7o%20para%20o%20libid%20gel.
+'><img src='https://upload.wikimedia.org/wikipedia/commons/thumb/f/f7/WhatsApp_logo.svg/1200px-WhatsApp_logo.svg.png'></img></a>
+
+   </div>
+  
+    </div>
+  );
+}
+
+export default App;
